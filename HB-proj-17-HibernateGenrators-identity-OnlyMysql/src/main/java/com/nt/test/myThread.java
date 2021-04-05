6134bdac91300a0841eb022dfbbc1b841201651a
My <My@AVINASH>
2021-04-05 23:19:02 +0530

    HibernateGenrators-identity-OnlyMysql

---
+++ HB-proj-17-HibernateGenrators-identity-OnlyMysql/src/main/java/com/nt/test/myThread.java
@@ -0,0 +1,34 @@
+package com.nt.test;
+
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
+import com.nt.entity.InsurancePolicy;
+import com.nt.utility.HibernateUtil;
+
+import javassist.bytecode.stackmap.BasicBlock.Catch;
+
+public class myThread extends Thread {
+	public void run() {
+	Transaction tx=null;
+	try(Session ses=HibernateUtil.getSession()){
+	tx=ses.beginTransaction();
+	InsurancePolicy policy= new InsurancePolicy();
+	policy.setPolicyName("Ja");
+	policy.setCompany("Lic");
+	policy.setTenure(5000.0f);
+	long idVal=(Long)ses.save(policy);	
+	System.out.println(idVal);
+	tx.commit();
+	
+	}//end try
+	catch(HibernateException hb){
+		hb.printStackTrace();
+	
+	}catch (Exception e) {
+	e.printStackTrace();
+
+	}
+}
+}
