d0eb049f01f3a083d6f7e4f36ada8d26b2515be5
My <My@AVINASH>
2021-04-12 20:51:35 +0530

    HibernateGenrators-Select

---
+++ HB-proj-18-HibernateGenrators-Select/src/main/java/com/nt/test/SaveObjectGenratorInClustorEnv.java
@@ -0,0 +1,45 @@
+package com.nt.test;
+
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+
+import com.nt.entity.InsurancePolicy;
+import com.nt.utility.HibernateUtil;
+
+public class SaveObjectGenratorInClustorEnv {
+
+	public static void main(String[] args) {
+		Transaction tx=null;
+		try(SessionFactory factory=HibernateUtil.getSessionFactory()){
+		     try(Session ses=HibernateUtil.getSession()){
+		
+		      //begin Transaction
+		     tx=ses.beginTransaction();
+			//Save Object
+			InsurancePolicy policy= new InsurancePolicy();
+		
+			policy.setPolicyName("lic");
+			policy.setCompany("js+++");
+			policy.setTenure(1500.0f);
+			 long idVal=(long)ses.save(policy);	
+		
+			System.out.println(idVal);
+			System.out.println(policy);
+			System.out.println("simulteneously.....Run the application");
+		Thread.sleep(40000);
+		tx.commit();
+System.out.println(idVal);
+		     }
+		}catch (HibernateException hb) {
+			hb.printStackTrace();
+			tx.rollback();
+		}
+		catch (Exception e) {
+		e.printStackTrace();
+		tx.rollback();
+		}	   
+	}
+
+}
