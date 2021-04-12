d8c5f5a189d3965a5fcce5bb911cedcfb1d810b4
My <My@AVINASH>
2021-04-12 20:52:30 +0530

    HibernateGenrators-guid

---
+++ HB-proj-19-HibernateGenrators-guid/src/main/java/com/nt/entity/InsurancePolicy.java
@@ -0,0 +1,17 @@
+package com.nt.entity;
+
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+import lombok.Data;
+@Data
+@Entity
+public class InsurancePolicy implements Serializable {
+
+private String policyId;
+private String policyName;
+private String company;
+private Float tenure;
+}
