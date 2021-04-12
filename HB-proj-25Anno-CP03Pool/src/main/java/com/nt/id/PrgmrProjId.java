25668a41ec7fdff20689d277e7982fad6e67d205
My <My@AVINASH>
2021-04-12 21:35:02 +0530

    JDBC-Conn-pool-CP03Pool

---
+++ HB-proj-25Anno-CP03Pool/src/main/java/com/nt/id/PrgmrProjId.java
@@ -0,0 +1,14 @@
+package com.nt.id;
+
+import java.io.Serializable;
+
+import javax.persistence.Embeddable;
+
+import lombok.Data;
+@Data
+@Embeddable
+public class PrgmrProjId implements Serializable {
+private Integer prgmrId;
+private Integer projId;
+
+}
